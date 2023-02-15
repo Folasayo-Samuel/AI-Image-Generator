d789621afbb7f1348e415d0d819f49c09f922714
Folasayo Samuel <folasayosamuel@gmail.com>
2023-02-15 13:38:01 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,11 @@ import { logo } from "./assets";
 import { Home, CreatePost } from "./pages";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]"></header>
+    </BrowserRouter>
+  );
 };
 
 export default App;
